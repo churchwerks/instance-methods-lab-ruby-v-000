b83e9ef0a7e82f8da83d58a792167a925f337714
churchwerks <steven.lennis@icloud.com>
2018-01-13 12:31:39 -0600

    Initialize

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+baxter = Dog.new
+killer = Dog.new
+honcho = Dog.new
+civilwerks = Dog.new
